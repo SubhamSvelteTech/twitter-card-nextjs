3be33f7878eb124403e042300eb89d8c599b460e
SubhamSvelteTech <shubham.sahu@sveltetech.com>
2024-11-06 10:59:33 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,24 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
- 
+
 export const metadata: Metadata = {
+  title: "Hi there!",
+  description: "Using opengraph in nextjs",
   openGraph: {
-    title: 'Hi there!',
-    description: 'Using opengraph in nextjs',
-    url: 'https://nextjs.org',
-    siteName: 'Next.js',
     images: [
       {
-        url: 'https://library.elementor.com/illustrator-portfolio/wp-content/uploads/sites/85/2020/06/faces-pic1.jpg',
+        url: "https://library.elementor.com/illustrator-portfolio/wp-content/uploads/sites/85/2020/06/faces-pic1.jpg",
         width: 800,
         height: 600,
       },
       {
-        url: 'https://res.cloudinary.com/omaha-code/w_1280,c_fill/omaha-code-cdn/2018/12/The-Open-Graph-Protocol-Guide.jpg',
+        url: "https://res.cloudinary.com/omaha-code/w_1280,c_fill/omaha-code-cdn/2018/12/The-Open-Graph-Protocol-Guide.jpg",
         width: 1800,
         height: 1600,
-        alt: 'My custom alt',
+        alt: "My custom alt",
       },
     ],
-    locale: 'en_US',
-    type: 'website',
   },
 };
 
