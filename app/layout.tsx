22af2fcc9186369cf19f9fdb75142a08ec43cc18
SubhamSvelteTech <shubham.sahu@sveltetech.com>
2024-11-07 17:11:55 +0530

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,27 @@
+import { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
-
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s",
+    default: "Title Main",
+  },
+  description: "qwerty qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty",
+  metadataBase: new URL("https://homeivf.in"),
+  twitter: {
+    card: "summary_large_image",
+    site: "homeivf.in",
+    creator: "Subh",
+    title: "qwerty ytrewq",
+    description: "Boost your Resume using AI and Land your Dream Job Today",
+    images: ["opengraph-image.png"],
+  },
+  openGraph: {
+    images: ["opengraph-image.png"],
+  },
+};
 
 export default function RootLayout({
   children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 // import { Metadata } from "next";
 
-import siteMetadata from "@/config/site-metadata";
-import { Metadata } from "next";
-
 // export const metadata: Metadata = {
 //   title: "HomeIVF | Bringing Life Home",
 //   description:
@@ -39,61 +36,6 @@ import { Metadata } from "next";
 //     ],
 //   },
 // };
-
-export const metadata: Metadata = {
-  title: {
-    default: siteMetadata.title,
-    template: `%s | ${siteMetadata.title}`,
-  },
-  metadataBase: new URL(
-    process.env.NEXT_PUBLIC_APP_URL ?? siteMetadata.siteUrl
-  ),
-  description: siteMetadata.description,
-  keywords: [
-    "Next.js",
-    "React",
-    "Tailwind CSS",
-    "James Shopland",
-    "Web Developer",
-    "Software Engineer",
-  ],
-  authors: [
-    {
-      name: "James Shopland",
-      url: "https://jamesshopland.com.com",
-    },
-  ],
-  creator: "James Shopland",
-  openGraph: {
-    type: "website",
-    locale: "en_GB",
-    url: siteMetadata.siteUrl,
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    siteName: siteMetadata.title,
-    images: [
-      {
-        url: `/og.jpg`,
-        width: 1200,
-        height: 630,
-        alt: siteMetadata.title,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteMetadata.title,
-    description: siteMetadata.description,
-    images: [`/og.png`],
-    creator: "@jollyshopland",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-  manifest: `/site.webmanifest`,
-}
 
 export default function Page() {
   return (
