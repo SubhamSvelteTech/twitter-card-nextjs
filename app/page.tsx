f5b10b1e210ec48490cca7c8af957c76a8d5d069
SubhamSvelteTech <shubham.sahu@sveltetech.com>
2024-11-08 11:59:47 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,45 @@
-// import { Metadata } from "next";
+import { Metadata } from "next";
 
 // export const metadata: Metadata = {
-//   title: "HomeIVF | Bringing Life Home",
-//   description:
-//     "HomeIVF is a platform that helps bring life home. Learn more about our services and how we can assist you.",
-//   keywords:
-//     "HomeIVF, IVF, fertility, fertility Family, Bringing Life Home, Home IVF Solutions, At-Home IVF Kits, Fertility Treatment at Home, IVF Success at Home, Home-Based Fertility Care, Family Fertility Plans, Personalized IVF Treatment, Family Planning with IVF, Affordable IVF at Home, Fertility Care Specialists, Conception at Home, Family Building with IVF, Home IVF Journey, Parenthood Dreams, Affordable Fertility Options, Empowering Fertility Choices, Home IVF Treatments, Affordable At-Home Fertility Solutions, Personalized Fertility Care at Home",
-//   openGraph: {
-//     title: "HI there",
-//     description: "Descr",
-//     url: "https://google.com",
-//     siteName: "HomeIVFG",
-//     images: [
-//       {
-//         url: "https://res.cloudinary.com/omaha-code/w_1280,c_fill/omaha-code-cdn/2018/12/The-Open-Graph-Protocol-Guide.jpg",
-//         width: 800,
-//         height: 600,
-//         alt: "HomeIVF",
-//       },
-//     ],
-//     locale: "en_US",
-//     type: "website",
+//   title: {
+//     template: "%s",
+//     default: "Title Main",
 //   },
+//   description: "qwerty qwertyqwertyqwertyqwertyqwertyqwertyqwertyqwertyqwerty",
+//   metadataBase: new URL("https://twitter-card-nextjs.vercel.app"),
 //   twitter: {
-//     card: "summary_large_image",
-//     title: "HomeIVF",
-//     description: "Descr",
-//     images: [
-//       {
-//         url: "https://res.cloudinary.com/omaha-code/w_1280,c_fill/omaha-code-cdn/2018/12/The-Open-Graph-Protocol-Guide.jpg",
-//         width: 800,
-//         height: 600,
-//         alt: "HomeIVF",
-//       },
-//     ],
+//     site: "twitter-card-nextjs.vercel.app",
+//     images: ["opengraph-image.png"],
+//   },
+//   openGraph: {
+//     images: ["opengraph-image.png"],
 //   },
 // };
+
+export const metadata: Metadata = {
+  title: "HomeIVF | Bringing Life Home",
+  description:
+    "HomeIVF is a platform that helps bring life home. Learn more about our services and how we can assist you.",
+  keywords:
+    "HomeIVF, IVF, fertility, fertility Family, Bringing Life Home, Home IVF Solutions, At-Home IVF Kits, Fertility Treatment at Home, IVF Success at Home, Home-Based Fertility Care, Family Fertility Plans, Personalized IVF Treatment, Family Planning with IVF, Affordable IVF at Home, Fertility Care Specialists, Conception at Home, Family Building with IVF, Home IVF Journey, Parenthood Dreams, Affordable Fertility Options, Empowering Fertility Choices, Home IVF Treatments, Affordable At-Home Fertility Solutions, Personalized Fertility Care at Home",
+
+  metadataBase: new URL("https://twitter-card-nextjs.vercel.app"),
+  twitter: {
+    card: "summary_large_image",
+    site: "@mongodb",  // Replace with your Twitter username
+    creator: "@mongodb", // Replace with the content creator's Twitter username
+    title: "HomeIVF | Bringing Life Home",
+    description:
+      "HomeIVF is a platform that helps bring life home. Learn more about our services and how we can assist you.",
+    images: ["https://twitter-card-nextjs.vercel.app/opengraph-image.png"],
+  },
+  openGraph: {
+    title: "HomeIVF | Bringing Life Home",
+    description:
+      "HomeIVF is a platform that helps bring life home. Learn more about our services and how we can assist you.",
+    images: ["https://twitter-card-nextjs.vercel.app/opengraph-image.png"],
+  },
+};
 
 export default function Page() {
   return (
